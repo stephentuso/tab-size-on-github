bf5b8c1e87aed37c13f5e649f423fff01aae1c92
stephentuso <tusodev@gmail.com>
2017-07-26 12:05:27 -0400

    Bring in new CSS selectors

--- background.js
+++ background.js
@@ -8,6 +8,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, updateInfo, tab) {
           .tab-size[data-tab-size='8'],
           .inline-review-comment,
           .gist table.lines,
+          table.diff-table,
+          .markdown-body pre,
+          body > pre,
           .blob-code {
             tab-size: ${items.tabSize || 4} !important;
           }
